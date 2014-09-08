e7cb5e9e97f462b103511c85ee6f06c4092d5f27
Arul Ramachandran <arulram@users.noreply.github.com>
2014-09-07 19:57:35 -0700

    Update pyquiz1.py

--- pyquiz1.py
+++ pyquiz1.py
@@ -1,5 +1,19 @@
 # Quiz: Cats with Hats
 # https://gist.github.com/mjhea0/113a48238b65a454f0b5
+#
+#Cats with Hats
+"""
+You have 100 cats.
+
+One day you decide to arrange all your cats in a giant circle. Initially, none of your cats have any hats on. You walk around the circle 100 times, always starting at the same spot, with the first cat (cat # 1). Every time you stop at a cat, you either put a hat on it if it doesn't have one on, or you take its hat off if it has one on.
+
+The first round, you stop at every cat, placing a hat on each one.
+The second round, you only stop at every second cat (#2, #4, #6, #8, etc.).
+The third round, you only stop at every third cat (#3, #6, #9, #12, etc.).
+You continue this process until you've made 100 rounds around the cats (e.g., you only visit the 100th cat).
+Write a program that simply outputs which cats have hats at the end.
+"""
+
 # The first round, you stop at every cat, placing a hat on each one
 
 list_of_cats = ['hat']*100
